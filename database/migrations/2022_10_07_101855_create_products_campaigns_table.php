cac23970017295bbdb7978b1e6ebc138f54feff2
dandevweb <danilo.pacheco@soitic.com>
2022-10-07 07:22:40 -0300

    Adicionado estrutura de banco de dados (Models - Migrations)

---
+++ database/migrations/2022_10_07_101855_create_products_campaigns_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products_campaigns', function (Blueprint $table) {
+            $table->foreignId('product_id')->nullable()->constrained();
+            $table->foreignId('campaign_id')->nullable()->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products_campaigns');
+    }
+};
