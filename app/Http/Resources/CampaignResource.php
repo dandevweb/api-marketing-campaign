8f29df855391ef4a5d27b3fa750eed491905380b
dandevweb <danilo.pacheco@soitic.com>
2022-10-08 12:20:57 -0300

    Inserido CRUD de Grupo de Cidades

--- app/Http/Resources/CampaignResource.php
+++ app/Http/Resources/CampaignResource.php
@@ -1,18 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class CampaignResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
-     */
-    public function toArray($request)
+
+    public function toArray($request): array
     {
         return parent::toArray($request);
     }
